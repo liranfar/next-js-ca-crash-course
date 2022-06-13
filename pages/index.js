fcb72a7d7a46820d8956375a273daf9a662d16fa
liranfar <l.farage@gmail.com>
2022-06-14 02:50:08 +0300

    articles design & refactor

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
 export default function Home({articles}) {
     console.log(articles)
@@ -9,6 +10,7 @@ export default function Home({articles}) {
             <meta name={'keywords'} content={'web, development, programming'}/>
         </Head>
       <h1>Welcome to next</h1>
+       <ArticleList articles={articles} />
     </div>
   )
 }
