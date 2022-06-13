ecf3be8be056a9d4d2f6c73e99677bc4f5c43a16
liranfar <l.farage@gmail.com>
2022-06-14 02:22:59 +0300

    conditional style

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Header = () => {
+        const x = 2;
         return (
             <div>
                 <h1 className='title'><span>WebDev</span> News</h1>
                 <style jsx>
                     {`
                       .title {
-                      color: red
+                      color: ${x > 3 ? 'red' : 'blue'}
                       }
                     `}
                 </style>
