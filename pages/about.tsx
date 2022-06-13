19889f763db4c43ddee00d4bd671aaeac4b7115c
liranfar <l.farage@gmail.com>
2022-06-14 01:16:03 +0300

    added head component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,12 @@
-const about = () => {
+import Head from "next/head";
+import * as React from "react";
+
+const about:React.FC = () => {
     return (
         <div>
+            <Head children={<h1>needs-children</h1>}>
+                <title>About</title>
+            </Head>
             <h1>About</h1>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+        <Head>
+            <title> WebDev Newz</title>
+            <meta name={'keywords'} content={'web, development, programming'}/>
+        </Head>
       <h1>Welcome to next</h1>
     </div>
   )
